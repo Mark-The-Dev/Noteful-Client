64eaa8b6078e7e944c70da3121d4e39d44a43202
Mark <SwiftTactics@gmail.com>
2020-09-28 17:28:41 -0400

    rendering issues

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -72,6 +72,6 @@ export default class Note extends React.Component {
 
 Note.propTypes = {
   name: PropTypes.string,
-  id: PropTypes.string,
+  id: PropTypes.number,
   modified: PropTypes.string
 }
